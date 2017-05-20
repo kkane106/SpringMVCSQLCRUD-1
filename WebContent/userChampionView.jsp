9db7288c8521bdd7622c8cfc5edf86058f047cf7
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-05-20 16:50:16 -0600

    Working on the controller

--- WebContent/userChampionView.jsp
+++ WebContent/userChampionView.jsp
@@ -7,13 +7,13 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>User Financial View</title>
+<title>Current League of Legends Champions!</title>
 </head>
 <body>
 
 <form method="GET" action="index.do"></form>
 
-<h2>User Financial View</h2>
+<h2>Current League of Legends Champions!</h2>
 
 </body>
 </html>
--- src/main/java/com/skilldistillery/controllers/ChampionController.java
+++ src/main/java/com/skilldistillery/controllers/ChampionController.java
@@ -1,10 +1,40 @@
 package com.skilldistillery.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.data.Champion;
+import com.skilldistillery.data.ChampionDAO;
 
 @Controller
-@SessionAttributes
+@SessionAttributes("champions")
 public class ChampionController {
-
+	@Autowired
+	private ChampionDAO championDAO;
+	
+	
+	
+	
+	@ModelAttribute("champions")
+	public ArrayList<Champion> pristineChampionsListDisplayedToUser() {
+		return new ArrayList<Champion>();
+	}
+	
+	
+	@RequestMapping(path="championList.do", method=RequestMethod.GET)
+	public ModelAndView listAllChampions() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("userChampionView.jsp");
+		mv.addObject("championsList", championDAO.getAllChampions());
+		return mv;
+		
+	}
+	
 }
